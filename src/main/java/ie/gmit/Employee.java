077e2b9b35ad0ba2a0882c17fee20ce076da019e
EVAN GARVEY - Student <G00322695@gmit.ie>
2020-03-09 10:38:07 +0000

    Commit!

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -1,4 +1,77 @@
 package ie.gmit;
 //Evan Garvey
-public class exam {
+//G00322695
+//In class exam (The Employee Class). Create arguements that cause a test to pass or fail
+//09/03/20
+
+public class Employee {
+
+    private String Title;
+    private String Name;
+    private String PPS;
+    private String Phone;
+    private String EmployType;
+   // private String Age;
+
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getPPS() {
+        return PPS;
+    }
+
+    public String getEmployType() {
+        return EmployType;
+    }
+
+//    public String getAge() {
+//        return Age;
+//    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public Employee(String Title, String Name, String PPS, String Phone, String EmployType, String Age) {
+
+        if (Title.equals("MR") || Title.equals("MRS") || Title.equals("MS")) {
+            this.Title = Title;
+        } else {
+            throw new IllegalArgumentException("Please enter one of the above titles:");
+        }
+
+        if (Name.length() >= 3 & Name.length() <= 25) {
+            this.Name = Name;
+        } else {
+            throw new IllegalArgumentException("Invalid name ERROR, try again.");
+        }
+        if (PPS.length() == 8) {
+            this.PPS = PPS;
+        } else {
+            throw new IllegalArgumentException("Invalid PPS ERROR, try again.");
+        }
+        if (Phone.length() == 9) {
+            this.Phone = Phone;
+        } else {
+            throw new IllegalArgumentException("Invalid phone number ERROR, try again.");
+        }
+        if (EmployType.equals("Full-Time") || EmployType.equals("Part-Time") || EmployType.equals("Contract")) {
+            this.EmployType = EmployType;
+        } else {
+            throw new IllegalArgumentException("Invalid Employment Type ERROR, try again.");
+        }
+//        if (Age >= 16) {
+//            throw new IllegalArgumentException("Age ERROR, Age must be over 16, try again.");
+//        } else {
+//            this.Age = Age;
+//        }
+
+
+    }
 }
--- src/test/java/ie/gmit/EmployeeTest.java
+++ src/test/java/ie/gmit/EmployeeTest.java
@@ -1,4 +1,53 @@
 package ie.gmit;
+//Evan Garvey
+//G00322695
+//In class exam (The Employee Class). Testing if there is incorrect data being inputted.
+//09/03/20
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EmployeeTest {
+
+    @BeforeEach
+    void setup() {
+        Employee myEmployee = new Employee("MR", "Evan Garvey", "12345678", "0123456789", "Full-time", "23");
+    }
+
+    @Test
+    void testConstructorNoTitle() {
+        // Employee myEmployee = new Employee("MR", "Evan Garvey", "12345678", "0123456789", "Full-time", "23");
+        assertThrows(IllegalArgumentException.class, () -> new Employee("", "Evan Garvey", "12345678", "0123456789", "Full-Time", "23"));
+    }
+
+    @Test
+    void testConstructorNoName() {
+        // Employee myEmployee = new Employee("MR", "Evan Garvey", "12345678", "0123456789", "Full-time", "23");
+        assertThrows(IllegalArgumentException.class, () -> new Employee("MR", "", "12345678", "0123456789", "Full-Time", "23"));
+    }
+
+    @Test
+    void testConstructorNoPPS() {
+        // Employee myEmployee = new Employee("MR", "Evan Garvey", "12345678", "0123456789", "Full-time", "23");
+        assertThrows(IllegalArgumentException.class, () -> new Employee("MR", "Evan Garvey", "", "0123456789", "Full-Time", "23"));
+    }
+    @Test
+    void testConstructorNoPhone() {
+        // Employee myEmployee = new Employee("MR", "Evan Garvey", "12345678", "0123456789", "Full-time", "23");
+        assertThrows(IllegalArgumentException.class, () -> new Employee("MR", "Evan Garvey", "12345678", "", "Full-Time", "23"));
+    }
+    @Test
+    void testConstructorNoEmployType() {
+        // Employee myEmployee = new Employee("MR", "Evan Garvey", "12345678", "0123456789", "Full-time", "23");
+        assertThrows(IllegalArgumentException.class, () -> new Employee("MR", "Evan Garvey", "12345678", "0123456789", "", "23"));
+    }
+    @Test
+    void testConstructorNoAge() {
+        assertThrows(IllegalArgumentException.class, () -> new Employee("MR", "Evan Garvey", "12345678", "0123456789", "Full-time", ""));
+    }
 }
+
+
+
